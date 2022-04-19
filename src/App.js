0b1b7991ccde32b1d7b27350a9729b96dab599d4
melinalentini@hotmail.com <mnlentini87@gmail.com>
2022-04-18 23:39:24 -0300

    Curso React
    Curso React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import DaisyNavBar from './components/DaisyNavBar';
 
 function App() {
   return (
     <div className="App">
+      <DaisyNavBar/>
+      <NavBar/>
+      <h1 className="text-3xl font-mono text-red-300">
+        Bienvenidos a Mundo Fibro
+      </h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <div>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Como comprar</li>
+            <li>Contacto</li>
+        </div>
+    )
+}
+export default NavBar
